d665b477cba78c527a170a0702542ec2de18801a
Andrew King <king140486@gmail.com>
2020-01-19 14:15:07 +0200

    Hw #10

--- Homework 10/script.js
+++ Homework 10/script.js
@@ -1,57 +1,45 @@
 'use strict';
 
 const calculator = createCalculator(10); 
-calculator.add(45) // возвращает 55 
-calculator.sub(45) // возвращает -35 
-calculator.div(5) // возвращает 2 
-calculator.mult(5) // возвращает 50 
-calculator.set(100) // устанавливает базовое значение в 100 
-calculator.mult(5) // возвращает 500
+calculator.add(45);
+calculator.sub(45);
+calculator.div(5);
+calculator.mult(5);
+calculator.set(100); 
+calculator.mult(5);
 
 function createCalculator(value) {
-  let result = value;
+  let basicValue = value;
 
   return {
-    add: function operationAdd(value) {
-      result += value;
+    add(operand) {
+      let result = basicValue + operand;
       console.log(result);
-    },
-    sub: function operationSub(value) {
-      result += value;
+      return result;     
+    },    
+    sub(operand) {
+      let result = basicValue - operand;
       console.log(result);
+      return result; 
     },
-    mult: function operationMult(value) {
-      result *= value;
+    mult(operand) {
+      let result = basicValue * operand;
       console.log(result);
+      return result; 
     },
-    div: function operationDiv(value) {
-      result /= value;
-      console.log(result);
+    div(operand) {
+      if (operand != 0) {
+        let result = basicValue / operand;
+        console.log(result);
+        return result; 
+      }
+      else {
+        console.log('Error: division by zero!');
+        return NaN;
+      }      
     },
-    set: function set(valToSet) {
-      result = valToSet;
+    set(newValue) {
+      basicValue = newValue;
     }
   }          
 }
-
-/*
-function operationAdd(value) {
-  result += value;
-  console.log(result);
-}
-
-function operationSub(value) {
-  result += value;
-  console.log(result);
-}
-
-function operationMult(value) {
-  result *= value;
-  console.log(result);
-}
-
-function operationDiv(value) {
-  result /= value;
-  console.log(result);
-}
-*/
