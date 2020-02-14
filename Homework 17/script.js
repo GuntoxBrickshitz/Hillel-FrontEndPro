b3674c1abd663208a7a7fe0cea458e43ee76e551
Andriy Korol <king140486@gmail.com>
2020-02-14 18:29:24 +0200

    Hw 17 v1.0

--- Homework 17/script.js
+++ Homework 17/script.js
@@ -1,119 +1,177 @@
 'use strict';
 
-const CLASS_TODO_ITEM = 'todo-item';
-const CLASS_TODO_ITEM_DONE = 'is-done';
-const CLASS_DEL_BUTTON = 'del-btn';
-const inputTask = document.querySelector('#inputTask');
-const addTaskForm = document.querySelector('#addTaskForm');
+const STICKER_ADD = 'add';
+const STICKER_HOLD = 'hold';
+const CLASS_STICKER_SHEET = 'sticker-sheet';
+const CLASS_STICKER_PANEL = 'sticker-panel';
+const CLASS_TEXT = 'text-field';
+const TEXT_COLUMNS = 16;
+const TEXT_ROWS = 10;
+const CLASS_STICKER_BUTTON = 'sticker-button';
 
-const taskList = document.querySelector('#taskList');
-const taskTemplate = document.querySelector('#taskTemplate').innerHTML;
+const ADD_BUTTON = {
+  label: 'Add',
+  type: 'add'
+};
+const SAVE_BUTTON = {
+  label: 'Save',
+  type: 'save'
+};
+const DEL_BUTTON = {
+  label: 'X',
+  type: 'delete'
+};
+const ADD_STICKER = {
+  id: '0',
+  text: '',
+  type: STICKER_ADD
+}
 
-let stickers;
+const stickerForm = document.querySelector('#stickerForm');
+const stickerTemplate = document.querySelector('#stickerTemplate').innerHTML;
 
-addTaskForm.addEventListener('submit', onSubmitAddTask);
-// taskList.addEventListener('click', onClickListItem);
+let stickers = [];
 
-// homework 14 starts here
+stickerForm.addEventListener('click', onSubmitStickerForm);
 
 start();
 
+function onSubmitStickerForm(event) {
+  const target = event.target;
+
+  event.preventDefault();
+
+  switch (true) {
+    case target.classList.contains(CLASS_STICKER_BUTTON):      
+
+      onClickButton(target, target.closest('.' + CLASS_STICKER_SHEET));
+      break;
+  }
+}
+
 function start() {
-  getStickers();  
+  getStickers();
+  showStickers();
 }
 
 function getStickers() {
-  stickers = localStorage.getItem('stickers');
+  const listFromStorage = localStorage.getItem('stickers');
+ 
+  stickers = JSON.parse(listFromStorage) || [ADD_STICKER];
 }
 
-function onSubmitAddTask(event) {
-  event.preventDefault();  
-  //processInput(); 
+function showStickers() {
+  stickerForm.innerHTML = stickers.map(makeStickerHtml).join('\n');
 }
 
-/*
+function makeStickerHtml(sticker) {
+  let button1Class = '';
+  let button1Label = '';
+  let button2Class = '';
+  let button2Label = '';
 
+  switch (sticker.type) {
+    case 'add':
+      button1Class = CLASS_STICKER_BUTTON;
+      button1Label = '';
+      button2Class = CLASS_STICKER_BUTTON + ' ' + ADD_BUTTON.type;
+      button2Label = ADD_BUTTON.label;
+      break;
 
+    default:
+      button1Class = CLASS_STICKER_BUTTON + ' ' + SAVE_BUTTON.type;
+      button1Label = SAVE_BUTTON.label;
+      button2Class = CLASS_STICKER_BUTTON + ' ' + DEL_BUTTON.type;
+      button2Label = DEL_BUTTON.label;
+      break;
+  }
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(response => {  
-  return response.json();
-})
-.then(listFromServer => {    
-  listFromServer.forEach(el => {    
-    const task = {
-      text: el.title,
-      done: el.completed,
+  return stickerTemplate
+      .replace('{{id}}', sticker.id)
+      .replace('{{textValue}}', sticker.text)
+      .replace('{{classSheet}}', CLASS_STICKER_SHEET + ' ' + sticker.type)
+      .replace('{{classPanel}}', CLASS_STICKER_PANEL)
+      .replace('{{classButton1}}', button1Class)
+      .replace('{{button1Label}}', button1Label)
+      .replace('{{classButton2}}', button2Class)
+      .replace('{{button2Label}}', button2Label)
+      .replace('{{classText}}', CLASS_TEXT)
+      .replace('{{textColumns}}', TEXT_COLUMNS)
+      .replace('{{textRows}}', TEXT_ROWS);
+}
+
+function onClickButton(button, stickerSheet) {
+  const buttonClasses = button.classList;
+  const stickerId = stickerSheet.dataset.id;
+  const text = getText(stickerSheet);
+
+  switch (true) {
+    case buttonClasses.contains(DEL_BUTTON.type):
+      console.log('delete');
+      deleteSticker(stickerId);
+      break;
+  
+    case text === '':
+      console.log('empty value');
+      return;
+
+    case buttonClasses.contains(ADD_BUTTON.type):
+      console.log('add');
+      addSticker(text);
+      break;
+
+    case buttonClasses.contains(SAVE_BUTTON.type):
+      console.log('save');
+      saveSticker(stickerId, text);
+      break;
+  }
+}
+
+function getText(stickerSheet) {
+  let item;
+
+  for (let i = 0; i < stickerSheet.children.length; i++) {
+    item = stickerSheet.children[i];
+
+    if (item.classList.contains(CLASS_TEXT)) {
+      return item.value;
     }
-    addTask(task);
-  });
-});
-
-// functions part
-
-function onSubmitAddTask(event) {
-  event.preventDefault();  
-  processInput(); 
-}
-
-function processInput() {
-  const task = {
-    text: inputTask.value,
-    done: false
   }
 
-  if (checkValue(task.text)) {
-    addTask(task);
+  return '';
+}
+
+function deleteSticker(stickerId) {
+  const index = stickers.findIndex(item => item.id === stickerId);
+
+  stickers.splice(index, 1);
+  saveStickersToStorage();
+  showStickers();
+}
+
+function addSticker(text) {
+  const timestamp = (new Date()).getTime().toString();
+  const newSticker = {
+    id: timestamp,
+    text: text,
+    type: STICKER_HOLD
   }
 
-  setFocusOnInput();
+  stickers.push(newSticker);
+  saveStickersToStorage();
+  showStickers();
 }
 
-function checkValue(value) {
-  return value != '';
+function saveSticker(stickerId, text) {
+  const index = stickers.findIndex(item => item.id === stickerId);
+
+  stickers[index].text = text;
+  saveStickersToStorage();
+  showStickers();
 }
 
-function addTask(task) {  
-  const taskItemHtml = taskTemplate.replace('{{text}}', task.text);
-  const taskItemElement = htmlToElement(taskItemHtml);
-
-  if (task.done) {
-    taskItemElement.classList.add(CLASS_TODO_ITEM_DONE);
-  }
-
-  taskList.appendChild(taskItemElement);
+function saveStickersToStorage() {
+  const listToStorage = JSON.stringify(stickers);
+  
+  localStorage.setItem('stickers', listToStorage);
 }
-
-function htmlToElement(html) {
-  const template = document.createElement('template');
-  html = html.trim();
-  template.innerHTML = html;
-  return template.content.firstChild;
-}
-
-function setFocusOnInput() {
-  inputTask.value = '';
-  inputTask.focus();
-}
-
-function onClickListItem(event) {  
-  const itemClass = event.target.classList;
-
-  if (itemClass.contains(CLASS_DEL_BUTTON)) {
-    removeItem(event.target.parentNode);
-  }
-  else
-  if (itemClass.contains(CLASS_TODO_ITEM)) {
-    toggleItem(itemClass);
-  }
-}
-
-function removeItem(element) {
-  element.remove();
-}
-
-function toggleItem(elementClass){
-  elementClass.toggle(CLASS_TODO_ITEM_DONE);
-}
-
-*/
